5551d1cb785ffedf2f525484fff4c62adc38de81
G4RDS <account@g4rds.jp>
2022-04-30 00:58:46 +0900

    Add sample style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import type { Component } from 'solid-js';
+import { container } from './App.css';
 
 export const App: Component = () => {
-  return <div></div>;
+  return <div class={container}>G4RDS</div>;
 };
