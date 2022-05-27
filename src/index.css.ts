cb787659373ba0bbbb66a54dfcfd6f7ec6c5cfa3
G4RDS <account@g4rds.jp>
2022-05-28 00:21:46 +0900

    Add success values

--- src/index.css.ts
+++ src/index.css.ts
@@ -61,6 +61,18 @@ export const themeVars = createGlobalTheme(':root', {
       800: '#1e293b',
       900: '#0f172a',
     },
+    success: {
+      50: '#f0fdf4',
+      100: '#dcfce7',
+      200: '#bbf7d0',
+      300: '#86efac',
+      400: '#4ade80',
+      500: '#22c55e',
+      600: '#16a34a',
+      700: '#15803d',
+      800: '#166534',
+      900: '#14532d',
+    },
   },
   breaks: {
     sm: '640px',
